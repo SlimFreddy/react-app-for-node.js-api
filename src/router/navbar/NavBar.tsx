03adaa4fe7c6c125d0153421b21385271979108d
Freddy <Frederik.Nies@btc-ag.com>
2021-02-26 13:34:27 +0100

    add sign out button to navbar

--- src/router/navbar/NavBar.tsx
+++ src/router/navbar/NavBar.tsx
@@ -1,35 +1,37 @@
 import React, { FC, useState } from "react";
-import { Navbar, Nav, Button, Form, Modal } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Navbar, Nav, Button, Form, Modal, Container } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 import SignIn from "../../components/SignIn";
 import { IRoute } from "../../models/IRoute";
+import LocalStorageService from "../../services/LocalStorageService";
 
 interface Props {
   routes: IRoute[];
 }
 const NavBar: FC<Props> = ({ routes }) => {
+  const history = useHistory();
+  const initialState = () => LocalStorageService.isLogged();
   const [showSignIn, setShowSignIn] = useState(false);
 
-  return (
-    <div>
-      <Navbar
-        sticky="top"
-        className="d-flex justify-content-between"
-        bg="dark"
-        variant="dark"
-      >
-        <Navbar.Brand>API</Navbar.Brand>
-        <Nav>
-          {routes.map((route) => {
-            return (
-              <Nav.Item key={route.name}>
-                <Nav.Link as={Link} to={route.path} eventKey={route.path}>
-                  {route.name}
-                </Nav.Link>
-              </Nav.Item>
-            );
-          })}
-        </Nav>
+  const buttonRenderer = () => {
+    if (initialState()) {
+      return (
+        <Form inline>
+          <Button
+            variant="success"
+            className="mx-2"
+            onClick={() => {
+              LocalStorageService.removeAuthInfo();
+              history.push("/");
+              window.location.reload();
+            }}
+          >
+            Sign Out
+          </Button>
+        </Form>
+      );
+    } else {
+      return (
         <Form inline>
           <Button
             variant="success"
@@ -50,6 +52,30 @@ const NavBar: FC<Props> = ({ routes }) => {
             Sign Up
           </Button>
         </Form>
+      );
+    }
+  };
+  return (
+    <div>
+      <Navbar
+        sticky="top"
+        className="d-flex justify-content-between"
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand>API</Navbar.Brand>
+        <Nav>
+          {routes.map((route) => {
+            return (
+              <Nav.Item key={route.name}>
+                <Nav.Link as={Link} to={route.path} eventKey={route.path}>
+                  {route.name}
+                </Nav.Link>
+              </Nav.Item>
+            );
+          })}
+        </Nav>
+        {buttonRenderer()}
       </Navbar>
     </div>
   );
